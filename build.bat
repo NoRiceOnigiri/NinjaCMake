daedfe6a104e0e3147e37ec0a048c44fa8e1d384
NoRiceOnigiri <nomorethensand@gmail.com>
2025-03-05 22:09:59 +0300

    Программа обучайка успешно собрана с помощью CMake.

---
+++ build.bat
@@ -0,0 +1,27 @@
+@echo off
+
+:: Переменные, хранящие тип сборки и суффикс для названия папок
+set BUILD_TYPE=Ninja
+set BUILD_SUFFIX=ninja
+
+:: Локализация
+chcp 65001
+
+:: Папки
+set BUILD_FOLDER=build_%BUILD_SUFFIX%
+set SOURCE_FOLDER=projects
+
+:: Создать папку с проектом, если её не существует
+if not exist %BUILD_FOLDER% mkdir %BUILD_FOLDER%
+
+:: Переход в директорию
+cd %BUILD_FOLDER%
+
+:: Сборка
+cmake -G %BUILD_TYPE% ..\%SOURCE_FOLDER%
+cmake --build .
+
+copy ..\%SOURCE_FOLDER%\obychaika\run_obychaika.bat .\obychaika
+
+:: На случай ошибки ошибки ошибки аааааааа
+pause
